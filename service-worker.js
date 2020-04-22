96974d61400921826c4dade667b7481546c65802
Kostya <oliinyk.kostya@gmail.com>
2020-04-22 21:54:24 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.64b2ba636afd4fdfaaa45a47f0335a41.js"
 );
 
 self.addEventListener('message', (event) => {
